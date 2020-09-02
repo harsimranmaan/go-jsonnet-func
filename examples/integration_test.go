ad6a54dd479f22505e1657c61081f0ff9fc6209b
Harsimran Singh Maan <maan.harry@gmail.com>
2020-09-01 17:59:14 -0700

    Fix test

--- examples/integration_test.go
+++ examples/integration_test.go
@@ -40,6 +40,6 @@ func ExampleSHA256() {
 	output, _ := vm.EvaluateSnippet("main.jsonnet", `local a = std.native("sha256")("test"); {out:a}`)
 	fmt.Println(output)
 	// Output: {
-	//    "ouat": "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+	//    "out": "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
 	// }
 }

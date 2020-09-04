f7f7582679e24bc0b66394f96ba3f013847cb2e1
Harsimran Singh Maan <maan.harry@gmail.com>
2020-09-04 13:54:24 -0700

    update example

--- examples/main.go
+++ examples/main.go
@@ -10,6 +10,9 @@ import (
 func main() {
 	vm := jsonnet.MakeVM()
 	vm.NativeFunction(gjf.SHA256())
-	output, _ := vm.EvaluateSnippet("main.jsonnet", `local a = std.native("sha256")("some string"); {out:a}`)
+	output, _ := vm.EvaluateSnippet("main.jsonnet", `local a = std.native("sha256")("test"); {out:a}`)
 	fmt.Println(output)
+	// Output: {
+	//    "out": "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+	// }
 }
